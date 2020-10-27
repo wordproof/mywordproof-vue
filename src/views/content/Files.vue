a3df03b0152bf7901094aa66a5f1a2cde1089074
Dennis Smit <frontenddeveloper@outlook.com>
2020-10-27 11:54:01 +0100

    Add hover extension for cards + new BaseIcon: downloadIcon

--- src/views/content/Files.vue
+++ src/views/content/Files.vue
@@ -1,7 +1,8 @@
-<template
-  ><!-- Infringement cards -->
-  <div class="mb-20">
-    <div class="flex">
+<template>
+  <!-- Infringement cards -->
+  <div class="h-screen">
+    <div class="flex" style="height:414px">
+      <!-- File upload -->
       <div class="w-1/4 p-2 mt-10">
         <div
           class="flex flex-col items-center justify-center text-center h-full max-w-sm rounded overflow-hidden bg-gray-400"
@@ -43,84 +44,266 @@
           </ul>
         </div>
       </div>
-      <div class="card">
-        <img
-          class="w-full"
-          src="../../../public/img/infringement4.png"
-          alt="Sunset in the mountains"
-        />
-        <div class="py-4">
-          <div class="font-bold text-base mb-2">
-            Liquicity Familyday 2020 artwork 1a
+      <!-- end of File upload -->
+
+      <!-- cards 1st row -->
+      <div class="card-container mt-10">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement4.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork 1a
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
           </div>
-          <p class="text-gray-600 text-xs">
-            Liquicity Familyday 2020 final RGB.jpg
-          </p>
-        </div>
-        <div class="hidden">ewkmdekwl</div>
-      </div>
-      <div class="card">
-        <img
-          class="w-full"
-          src="../../../public/img/infringement3.png"
-          alt="Sunset in the mountains"
-        />
-        <div class="py-4">
-          <div class="font-bold text-base mb-2">
-            Liquicity Familyday 2020 artwork
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
           </div>
-          <p class="text-gray-600 text-xs">
-            Liquicity Familyday 2020 final RGB.jpg
-          </p>
-        </div>
-      </div>
-      <div class="card">
-        <img
-          class="w-full"
-          src="../../../public/img/infringement4.png"
-          alt="Sunset in the mountains"
-        />
-        <div class="py-4">
-          <div class="font-bold text-base mb-2">
-            Liquicity Familyday 2020 artwork
-          </div>
-          <p class="text-gray-600 text-xs">
-            Liquicity Familyday 2020 final RGB.jpg
-          </p>
         </div>
       </div>
+      <div class="card-container mt-10">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement3.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
+          </div>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-container mt-10">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement4.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
+          </div>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- end of cards -->
     </div>
     <div class="flex">
-      <div class="w-1/4 max-w-sm rounded overflow-hidden mt-10">
-        <img
-          class="w-full"
-          src="../../../public/img/infringement5.png"
-          alt="Sunset in the mountains"
-        />
-        <div class="py-4">
-          <div class="font-bold text-base mb-2">
-            Liquicity Familyday 2020 artwork
+      <!-- cards 2e row -->
+      <div class="card-container">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement4.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
           </div>
-          <p class="text-gray-600 text-xs">
-            Liquicity Familyday 2020 final RGB.jpg
-          </p>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
         </div>
       </div>
-      <div class="w-1/4 max-w-sm rounded overflow-hidden mt-10">
-        <img
-          class="w-full"
-          src="../../../public/img/infringement-image-1.png"
-          alt="Sunset in the mountains"
-        />
-        <div class="py-4">
-          <div class="font-bold text-base mb-2">
-            Liquicity Familyday 2020 artwork
+      <div class="card-container">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement5.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
           </div>
-          <p class="text-gray-600 text-xs">
-            Liquicity Familyday 2020 final RGB.jpg
-          </p>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
         </div>
       </div>
+      <div class="card-container">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement4.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
+          </div>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="card-container">
+        <div class="card">
+          <img
+            class="w-full"
+            src="../../../public/img/infringement5.png"
+            alt="Sunset in the mountains"
+          />
+          <div class="py-4">
+            <div class="font-bold text-base mb-2">
+              Liquicity Familyday 2020 artwork
+            </div>
+            <p class="text-gray-600 text-xs">
+              Liquicity Familyday 2020 final RGB.jpg
+            </p>
+          </div>
+          <div class="card-footer invisible opacity-0">
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="downloadIcon"
+                width="23px"
+                height="23px"
+              />
+            </button>
+
+            <button>
+              <BaseIcon
+                class="text-blue"
+                name="dotsIcon"
+                width="5px"
+                height="25px"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+      <!-- end of cards -->
     </div>
   </div>
 </template>
@@ -149,10 +332,21 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.card-container {
+  @apply relative w-1/4;
+}
 .card {
-  @apply w-1/4 max-w-sm rounded overflow-hidden mt-10 p-2;
+  max-height: 414px;
+  @apply absolute max-w-sm rounded p-2;
   &:hover {
     box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.16);
+    z-index: 100;
+    background-color: #fff;
+    .card-footer {
+      bottom: 16px;
+      transition: visibility 0.3s linear, opacity 0.3s linear;
+      @apply visible flex justify-between relative text-blue mt-8 px-1 opacity-100;
+    }
   }
 }
 </style>
